e3f372bb4899c1bca0c8ad219629b15e92f759c9
RayaneSlimani <rayane.slimani@epitech.eu>
2018-03-06 14:12:31 +0100

    htaccess

--- Core/autoload.php
+++ Core/autoload.php
@@ -5,12 +5,9 @@ class Autoloader {
     static function register(){
         spl_autoload_register(array(__CLASS__, 'autoload'));
     }
-
-    // Inclue le fichier correspondant à notre classe
     static function autoload($class){
         $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
         require_once($path . '.php');
     }
-    
 }
 Autoloader::register();
